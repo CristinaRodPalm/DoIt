0c912b411cf75ffdaadd60e9b206605531fb2a07
Julian <julian.egea.ponce@gmail.com>
2017-02-23 19:03:49 +0100

    Empezamos con el ui-router.

---
+++ src/main/webapp/app_extra_julian_david/app_extra.js
@@ -0,0 +1,29 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('doitApp')
+        .config(stateConfig);
+
+    stateConfig.$inject = ['$stateProvider'];
+
+    function stateConfig($stateProvider) {
+        $stateProvider.state('app', {
+            abstract: true,
+            views: {
+                'navbar@': {
+                    templateUrl: 'app_extra_julian_david/navbar/navbar.html',
+                    controller: 'navbar.controller',
+                    controllerAs: 'vm'
+                }
+            },
+            resolve: {
+                authorize: ['Auth',
+                    function (Auth) {
+                        return Auth.authorize();
+                    }
+                ]
+            }
+        });
+    }
+})();
