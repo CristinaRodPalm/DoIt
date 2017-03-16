b7d927aedc539765ecdf705aecf8601a5eaa1845
Cristina <cristinarodriguez.palmer@gmail.com>
2017-03-16 19:29:54 +0100

    #10 login activo en la home del proyecto, si se conecta no se muestra el form de login
    Signed-off-by: Cristina <cristinarodriguez.palmer@gmail.com>

--- src/main/webapp/app/home/home.controller.js
+++ src/main/webapp/app/home/home.controller.js
@@ -5,14 +5,15 @@
         .module('doitApp')
         .controller('HomeController', HomeController);
 
-    HomeController.$inject = ['$scope', 'Principal', 'LoginService', '$state'];
+    HomeController.$inject = ['$rootScope', '$scope', 'Principal', 'Auth','LoginService', '$state'];
 
-    function HomeController ($scope, Principal, LoginService, $state) {
+    function HomeController ($rootScope, $scope, Principal, Auth, $state) {
         var vm = this;
 
         vm.account = null;
         vm.isAuthenticated = null;
-        vm.login = LoginService.open;
+       // vm.login = LoginService.open;
+        vm.login = login;
         vm.register = register;
         $scope.$on('authenticationSuccess', function() {
             getAccount();
@@ -28,6 +29,34 @@
         }
         function register () {
             $state.go('register');
+        }
+
+        function login (event) {
+            event.preventDefault();
+            Auth.login({
+                username: vm.username,
+                password: vm.password,
+                rememberMe: vm.rememberMe
+            }).then(function () {
+                vm.authenticationError = false;
+                /*$uibModalInstance.close();
+                if ($state.current.name === 'register' || $state.current.name === 'activate' ||
+                    $state.current.name === 'finishReset' || $state.current.name === 'requestReset') {
+                    $state.go('home');
+                }*/
+
+                $rootScope.$broadcast('authenticationSuccess');
+
+                // previousState was set in the authExpiredInterceptor before being redirected to login modal.
+                // since login is successful, go to stored previousState and clear previousState
+                if (Auth.getPreviousState()) {
+                    var previousState = Auth.getPreviousState();
+                    Auth.resetPreviousState();
+                    $state.go(previousState.name, previousState.params);
+                }
+            }).catch(function () {
+                vm.authenticationError = true;
+            });
         }
     }
 })();
