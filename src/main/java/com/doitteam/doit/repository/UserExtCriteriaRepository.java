c50daf1afe0598a5c6e7246ecbb01ade561c6e8b
Cristina <cristinarodriguez.palmer@gmail.com>
2017-03-20 19:07:33 +0100

    #7 buscador por like hecho
    Signed-off-by: Cristina <cristinarodriguez.palmer@gmail.com>

--- src/main/java/com/doitteam/doit/repository/UserExtCriteriaRepository.java
+++ src/main/java/com/doitteam/doit/repository/UserExtCriteriaRepository.java
@@ -3,15 +3,14 @@ package com.doitteam.doit.repository;
 import com.doitteam.doit.domain.User;
 import com.doitteam.doit.domain.UserExt;
 import com.doitteam.doit.domain.UserExt_;
+import com.doitteam.doit.domain.User_;
 import org.springframework.stereotype.Repository;
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Join;
-import javax.persistence.criteria.Root;
+import javax.persistence.criteria.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -41,25 +40,27 @@ public class UserExtCriteriaRepository {
         String lastName = (String) parameters.get("lastName");
         String email = (String) parameters.get("email");
 
-
-        if(login != null) filterByLogin(parameters, userExtCriteriaQuery, builder);
+        if(login != null) filterByLogin(parameters, userExtCriteriaQuery, userExtRoot, builder);
         if(telefono != null) filterByTlf(parameters, userExtCriteriaQuery, userExtRoot, builder);
         if(firstName != null) filterByFirstName(parameters, userExtCriteriaQuery, builder);
         if(lastName != null) filterByLastName(parameters, userExtCriteriaQuery, builder);
         if(email != null) filterByEmail(parameters, userExtCriteriaQuery, builder);
     }
-    private void filterByLogin(Map<String, Object> parameters, CriteriaQuery<UserExt> userExtCriteriaQuery, CriteriaBuilder builder){
-        String searchLogin = (String) parameters.get("login");
 
-        Root<User> user = userExtCriteriaQuery.from(User.class);
-        Join<User, UserExt> userExt = user.join("userExt");
-        userExtCriteriaQuery.select(userExt).where(builder.equal(user.get("login"), searchLogin)).distinct(true);
-    }
-
+    // Atributo de UserExt
     private void filterByTlf(Map<String, Object> parameters, CriteriaQuery<UserExt> userExtCriteriaQuery, Root<UserExt> userExtRoot, CriteriaBuilder builder){
         String searchTelf = (String) parameters.get("telefono");
         userExtCriteriaQuery.where(builder.like(userExtRoot.get(UserExt_.telefono),
             "%"+searchTelf+"%"));
+    }
+
+    //Atributo de User
+    private void filterByLogin(Map<String, Object> parameters, CriteriaQuery<UserExt> userExtCriteriaQuery, Root<UserExt> userExtRoot, CriteriaBuilder builder){
+        String login = (String) parameters.get("login");
+
+        Root<User> user = userExtCriteriaQuery.from(User.class);
+        Join<User, UserExt> userExt = user.join("userExt");
+        userExtCriteriaQuery.select(userExt).where(builder.like(user.get("login"), login)).distinct(true);
     }
 
     private void filterByFirstName(Map<String, Object> parameters, CriteriaQuery<UserExt> userExtCriteriaQuery, CriteriaBuilder builder){
@@ -67,7 +68,7 @@ public class UserExtCriteriaRepository {
 
         Root<User> user = userExtCriteriaQuery.from(User.class);
         Join<User, UserExt> userExt = user.join("userExt");
-        userExtCriteriaQuery.select(userExt).where(builder.equal(user.get("firstName"), firstName)).distinct(true);
+        userExtCriteriaQuery.select(userExt).where(builder.like(user.get("firstName"), firstName)).distinct(true);
     }
 
     private void filterByLastName(Map<String, Object> parameters, CriteriaQuery<UserExt> userExtCriteriaQuery, CriteriaBuilder builder){
@@ -75,7 +76,7 @@ public class UserExtCriteriaRepository {
 
         Root<User> user = userExtCriteriaQuery.from(User.class);
         Join<User, UserExt> userExt = user.join("userExt");
-        userExtCriteriaQuery.select(userExt).where(builder.equal(user.get("lastName"), lastName)).distinct(true);
+        userExtCriteriaQuery.select(userExt).where(builder.like(user.get("lastName"), lastName)).distinct(true);
     }
 
     private void filterByEmail(Map<String, Object> parameters, CriteriaQuery<UserExt> userExtCriteriaQuery, CriteriaBuilder builder){
@@ -83,6 +84,38 @@ public class UserExtCriteriaRepository {
 
         Root<User> user = userExtCriteriaQuery.from(User.class);
         Join<User, UserExt> userExt = user.join("userExt");
-        userExtCriteriaQuery.select(userExt).where(builder.equal(user.get("email"), email)).distinct(true);
+        userExtCriteriaQuery.select(userExt).where(builder.like(user.get("email"), email)).distinct(true);
     }
+
+/*
+    public List<UserExt> filterText(Map<String, Object> parameters){
+        CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<UserExt> userExtCriteriaQuery = builder.createQuery(UserExt.class);
+
+        Subquery<Long> subquery = userExtCriteriaQuery.subquery(Long.class);
+        Root<User> subRoot = subquery.from(User.class);
+        subquery.select(builder.literal(1L));
+
+        List<Predicate> predicates = new ArrayList<Predicate>();
+
+        ParameterExpression<String> login = builder.parameter(String.class);
+        ParameterExpression<String> firstName = builder.parameter(String.class);
+        Predicate exp1 = builder.equal(subRoot.get(User_.login), login);
+        Predicate exp2 = builder.equal(subRoot.get(User_.firstName), firstName);
+        Predicate and = builder.and(exp1, exp2);
+
+        predicates.add(and);
+
+        String log = (String) parameters.get("login");
+        String first = (String) parameters.get("firstName");
+
+        subquery.where(predicates.toArray(new Predicate[0]));
+        userExtCriteriaQuery.where(builder.exists(subquery).not());
+
+        return entityManager.createQuery(userExtCriteriaQuery)
+            .setParameter(login, log)
+            .setParameter(firstName, first)
+            .getResultList();
+
+    }*/
 }
