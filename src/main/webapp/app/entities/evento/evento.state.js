55faa955e3cbdfd2682b5725b5a0f1fbce2cae6d
Julian <julian.egea.ponce@gmail.com>
2017-03-20 18:55:22 +0100

    Pendiente de aceptar y rechazar solicitudes de amistad. Pendiente la creación de eventos.

--- src/main/webapp/app/entities/evento/evento.state.js
+++ src/main/webapp/app/entities/evento/evento.state.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
     'use strict';
 
     angular
@@ -9,161 +9,176 @@
 
     function stateConfig($stateProvider) {
         $stateProvider
-        .state('evento', {
-            parent: 'entity',
-            url: '/evento',
-            data: {
-                authorities: ['ROLE_USER'],
-                pageTitle: 'Eventos'
-            },
-            views: {
-                'content@': {
-                    templateUrl: 'app/entities/evento/eventos.html',
-                    controller: 'EventoController',
-                    controllerAs: 'vm'
-                }
-            },
-            resolve: {
-            }
-        })
-        .state('evento-detail', {
-            parent: 'evento',
-            url: '/evento/{id}',
-            data: {
-                authorities: ['ROLE_USER'],
-                pageTitle: 'Evento'
-            },
-            views: {
-                'content@': {
-                    templateUrl: 'app/entities/evento/evento-detail.html',
-                    controller: 'EventoDetailController',
-                    controllerAs: 'vm'
-                }
-            },
-            resolve: {
-                entity: ['$stateParams', 'Evento', function($stateParams, Evento) {
-                    return Evento.get({id : $stateParams.id}).$promise;
-                }],
-                previousState: ["$state", function ($state) {
-                    var currentStateData = {
-                        name: $state.current.name || 'evento',
-                        params: $state.params,
-                        url: $state.href($state.current.name, $state.params)
-                    };
-                    return currentStateData;
-                }]
-            }
-        })
-        .state('evento-detail.edit', {
-            parent: 'evento-detail',
-            url: '/detail/edit',
-            data: {
-                authorities: ['ROLE_USER']
-            },
-            onEnter: ['$stateParams', '$state', '$uibModal', function($stateParams, $state, $uibModal) {
-                $uibModal.open({
-                    templateUrl: 'app/entities/evento/evento-dialog.html',
-                    controller: 'EventoDialogController',
-                    controllerAs: 'vm',
-                    backdrop: 'static',
-                    size: 'lg',
-                    resolve: {
-                        entity: ['Evento', function(Evento) {
-                            return Evento.get({id : $stateParams.id}).$promise;
-                        }]
+            .state('evento', {
+                parent: 'entity',
+                url: '/evento',
+                data: {
+                    authorities: ['ROLE_USER'],
+                    pageTitle: 'Eventos'
+                },
+                views: {
+                    'content@': {
+                        templateUrl: 'app/entities/evento/eventos.html',
+                        controller: 'EventoController',
+                        controllerAs: 'vm'
                     }
-                }).result.then(function() {
-                    $state.go('^', {}, { reload: false });
-                }, function() {
-                    $state.go('^');
-                });
-            }]
-        })
-        .state('evento.new', {
-            parent: 'evento',
-            url: '/new',
-            data: {
-                authorities: ['ROLE_USER']
-            },
-            onEnter: ['$stateParams', '$state', '$uibModal', function($stateParams, $state, $uibModal) {
-                $uibModal.open({
-                    templateUrl: 'app/entities/evento/evento-dialog.html',
-                    controller: 'EventoDialogController',
-                    controllerAs: 'vm',
-                    backdrop: 'static',
-                    size: 'lg',
-                    resolve: {
-                        entity: function () {
-                            return {
-                                nombre: null,
-                                descripcion: null,
-                                hora: null,
-                                imagen: null,
-                                imagenContentType: null,
-                                urlMaps: null,
-                                latitud: null,
-                                longitud: null,
-                                fechaEvento: null,
-                                id: null
-                            };
+                },
+                resolve: {}
+            })
+            .state('evento-detail', {
+                parent: 'evento',
+                url: '/evento/{id}',
+                data: {
+                    authorities: ['ROLE_USER'],
+                    pageTitle: 'Evento'
+                },
+                views: {
+                    'content@': {
+                        templateUrl: 'app/entities/evento/evento-detail.html',
+                        controller: 'EventoDetailController',
+                        controllerAs: 'vm'
+                    }
+                },
+                resolve: {
+                    entity: ['$stateParams', 'Evento', function ($stateParams, Evento) {
+                        return Evento.get({id: $stateParams.id}).$promise;
+                    }],
+                    previousState: ["$state", function ($state) {
+                        var currentStateData = {
+                            name: $state.current.name || 'evento',
+                            params: $state.params,
+                            url: $state.href($state.current.name, $state.params)
+                        };
+                        return currentStateData;
+                    }]
+                }
+            })
+            .state('evento-detail.edit', {
+                parent: 'evento-detail',
+                url: '/detail/edit',
+                data: {
+                    authorities: ['ROLE_USER']
+                },
+                onEnter: ['$stateParams', '$state', '$uibModal', function ($stateParams, $state, $uibModal) {
+                    $uibModal.open({
+                        templateUrl: 'app/entities/evento/evento-dialog.html',
+                        controller: 'EventoDialogController',
+                        controllerAs: 'vm',
+                        backdrop: 'static',
+                        size: 'lg',
+                        resolve: {
+                            entity: ['Evento', function (Evento) {
+                                return Evento.get({id: $stateParams.id}).$promise;
+                            }]
                         }
+                    }).result.then(function () {
+                        $state.go('^', {}, {reload: false});
+                    }, function () {
+                        $state.go('^');
+                    });
+                }]
+            })
+            .state('evento.new', {
+                parent: 'evento',
+                url: '/new',
+                data: {
+                    authorities: ['ROLE_USER']
+                },
+                onEnter: ['$stateParams', '$state', '$uibModal', function ($stateParams, $state, $uibModal) {
+                    $uibModal.open({
+                        templateUrl: 'app/entities/evento/evento-dialog.html',
+                        controller: 'EventoDialogController',
+                        controllerAs: 'vm',
+                        backdrop: 'static',
+                        size: 'lg',
+                        resolve: {
+                            entity: function () {
+                                return {
+                                    nombre: null,
+                                    descripcion: null,
+                                    hora: null,
+                                    imagen: null,
+                                    imagenContentType: null,
+                                    urlMaps: null,
+                                    latitud: null,
+                                    longitud: null,
+                                    fechaEvento: null,
+                                    id: null
+                                };
+                            }
+                        }
+                    }).result.then(function () {
+                        $state.go('evento', null, {reload: 'evento'});
+                    }, function () {
+                        $state.go('evento');
+                    });
+                }]
+            })
+            .state('evento.edit', {
+                parent: 'evento',
+                url: '/{id}/edit',
+                data: {
+                    authorities: ['ROLE_USER']
+                },
+                onEnter: ['$stateParams', '$state', '$uibModal', function ($stateParams, $state, $uibModal) {
+                    $uibModal.open({
+                        templateUrl: 'app/entities/evento/evento-dialog.html',
+                        controller: 'EventoDialogController',
+                        controllerAs: 'vm',
+                        backdrop: 'static',
+                        size: 'lg',
+                        resolve: {
+                            entity: ['Evento', function (Evento) {
+                                return Evento.get({id: $stateParams.id}).$promise;
+                            }]
+                        }
+                    }).result.then(function () {
+                        $state.go('evento', null, {reload: 'evento'});
+                    }, function () {
+                        $state.go('^');
+                    });
+                }]
+            })
+            .state('evento.delete', {
+                parent: 'evento',
+                url: '/{id}/delete',
+                data: {
+                    authorities: ['ROLE_USER']
+                },
+                onEnter: ['$stateParams', '$state', '$uibModal', function ($stateParams, $state, $uibModal) {
+                    $uibModal.open({
+                        templateUrl: 'app/entities/evento/evento-delete-dialog.html',
+                        controller: 'EventoDeleteController',
+                        controllerAs: 'vm',
+                        size: 'md',
+                        resolve: {
+                            entity: ['Evento', function (Evento) {
+                                return Evento.get({id: $stateParams.id}).$promise;
+                            }]
+                        }
+                    }).result.then(function () {
+                        $state.go('evento', null, {reload: 'evento'});
+                    }, function () {
+                        $state.go('^');
+                    });
+                }]
+            })
+            .state('eventos', {
+                parent: 'entity',
+                url: '/lista-eventos',
+                data: {
+                    authorities: ['ROLE_USER'],
+                    pageTitle: 'Eventos'
+                },
+                views: {
+                    'content@': {
+                        templateUrl: 'app/entities/evento/lista-eventos.html',
+                        controller: 'EventoController',
+                        controllerAs: 'vm'
                     }
-                }).result.then(function() {
-                    $state.go('evento', null, { reload: 'evento' });
-                }, function() {
-                    $state.go('evento');
-                });
-            }]
-        })
-        .state('evento.edit', {
-            parent: 'evento',
-            url: '/{id}/edit',
-            data: {
-                authorities: ['ROLE_USER']
-            },
-            onEnter: ['$stateParams', '$state', '$uibModal', function($stateParams, $state, $uibModal) {
-                $uibModal.open({
-                    templateUrl: 'app/entities/evento/evento-dialog.html',
-                    controller: 'EventoDialogController',
-                    controllerAs: 'vm',
-                    backdrop: 'static',
-                    size: 'lg',
-                    resolve: {
-                        entity: ['Evento', function(Evento) {
-                            return Evento.get({id : $stateParams.id}).$promise;
-                        }]
-                    }
-                }).result.then(function() {
-                    $state.go('evento', null, { reload: 'evento' });
-                }, function() {
-                    $state.go('^');
-                });
-            }]
-        })
-        .state('evento.delete', {
-            parent: 'evento',
-            url: '/{id}/delete',
-            data: {
-                authorities: ['ROLE_USER']
-            },
-            onEnter: ['$stateParams', '$state', '$uibModal', function($stateParams, $state, $uibModal) {
-                $uibModal.open({
-                    templateUrl: 'app/entities/evento/evento-delete-dialog.html',
-                    controller: 'EventoDeleteController',
-                    controllerAs: 'vm',
-                    size: 'md',
-                    resolve: {
-                        entity: ['Evento', function(Evento) {
-                            return Evento.get({id : $stateParams.id}).$promise;
-                        }]
-                    }
-                }).result.then(function() {
-                    $state.go('evento', null, { reload: 'evento' });
-                }, function() {
-                    $state.go('^');
-                });
-            }]
-        });
+                },
+                resolve: {}
+            });
     }
 
 })();
