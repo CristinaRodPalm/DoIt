00186725bcaf7a0c44c0d12a31e4c7d1b8e8b7fd
Julian <juegpostucom95>
2017-05-08 12:12:12 +0200

    Ahora solo el admin puede editar el evento dentro de los detalles del mismo.

--- src/main/webapp/app/entities/evento/evento-detail.controller.js
+++ src/main/webapp/app/entities/evento/evento-detail.controller.js
@@ -5,19 +5,24 @@
         .module('doitApp')
         .controller('EventoDetailController', EventoDetailController);
 
-    EventoDetailController.$inject = ['$scope', '$rootScope', '$stateParams', 'previousState', 'DataUtils', 'entity', 'Evento', 'Reto', 'User', 'InvitacionEvento', 'Chat'];
+    EventoDetailController.$inject = ['Principal', '$scope', '$rootScope', '$stateParams', 'previousState', 'DataUtils', 'entity', 'Evento', 'Reto', 'User', 'InvitacionEvento', 'Chat'];
 
-    function EventoDetailController($scope, $rootScope, $stateParams, previousState, DataUtils, entity, Evento, Reto, User, InvitacionEvento, Chat) {
+    function EventoDetailController(Principal, $scope, $rootScope, $stateParams, previousState, DataUtils, entity, Evento, Reto, User, InvitacionEvento, Chat) {
         var vm = this;
 
         vm.evento = entity;
         vm.previousState = previousState.name;
         vm.byteSize = DataUtils.byteSize;
         vm.openFile = DataUtils.openFile;
-
+        vm.currentAccount;
         var unsubscribe = $rootScope.$on('doitApp:eventoUpdate', function(event, result) {
             vm.evento = result;
         });
+
+        Principal.identity().then(function(account) {
+            vm.currentAccount = account;
+        });
+
         $scope.$on('$destroy', unsubscribe);
     }
 })();
