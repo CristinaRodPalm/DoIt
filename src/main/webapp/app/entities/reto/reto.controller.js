ca704149054b19d96e500ffcb32813771060c8bf
Cristina <cristinarodriguez.palmer@gmail.com>
2017-05-09 19:11:20 +0200

    Se abre el modal con la foto
    Signed-off-by: Cristina <cristinarodriguez.palmer@gmail.com>

--- src/main/webapp/app/entities/reto/reto.controller.js
+++ src/main/webapp/app/entities/reto/reto.controller.js
@@ -5,9 +5,9 @@
         .module('doitApp')
         .controller('RetoController', RetoController);
 
-    RetoController.$inject = ['DataUtils', 'Reto', 'Principal'];
+    RetoController.$inject = ['DataUtils', 'Reto', 'Principal', '$uibModal'];
 
-    function RetoController(DataUtils, Reto, Principal) {
+    function RetoController(DataUtils, Reto, Principal, $uibModal) {
 
         var vm = this;
 
@@ -15,6 +15,7 @@
         vm.openFile = DataUtils.openFile;
         vm.byteSize = DataUtils.byteSize;
         vm.currentAccount;
+        vm.openModal = openModal;
 
         loadAll();
 
@@ -28,5 +29,12 @@
         Principal.identity().then(function(account) {
             vm.currentAccount = account;
         });
+
+        // abrir modal con la foto
+        function openModal(data){
+            var modalInstance = $uibModal.open({
+                template: '<img style="heigth:100%;width:100%" data-ng-src="'+'data:'+data.imageContentType+';base64,'+data.imagen+'"</img>'
+            });
+        }
     }
 })();
